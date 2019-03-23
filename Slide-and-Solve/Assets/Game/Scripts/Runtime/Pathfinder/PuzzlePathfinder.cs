bf54181544a6935f58920134b7f580212b8a3d4f
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-03-23 19:39:35 +0100

    Small refactoring; Added comments and documentation;

--- Slide-and-Solve/Assets/Game/Scripts/Runtime/Pathfinder/PuzzlePathfinder.cs
+++ Slide-and-Solve/Assets/Game/Scripts/Runtime/Pathfinder/PuzzlePathfinder.cs
@@ -18,17 +18,18 @@ namespace Wokarol.Pathfinder
             _statesToGraphMap = new Dictionary<StateT, Node>();
             _graph = new List<Node>();
             GenerateNodes(processor, initialState, possibleMoves, out List<List<StateT>> neighbourMap, out List<List<MoveT>> neighbourMovesMap);
-            FindNodeNeighbours(neighbourMap, neighbourMovesMap, possibleMoves);
-
+            FindNodeNeighbours(neighbourMap, neighbourMovesMap);
             return this;
         }
 
         /// <summary>
         /// Creates nodes from posibble states
         /// </summary>
-        /// <param name="processor"></param>
-        /// <param name="initialState"></param>
-        /// <param name="possibleMoves"></param>
+        /// <param name="processor">Puzzle processor</param>
+        /// <param name="initialState">Initial position to test all possible puzzle states</param>
+        /// <param name="possibleMoves">List of moves that can be performed</param>
+        /// <param name="neighbourMap">Map that corresponds to all states that are neighbour to given node, is same order as _graph</param>
+        /// <param name="neighbourMovesMap">Map that correcponds to all moves needed to get to neighbour states, same sizes as neighbourMap</param>
         /// <returns>List of neighbouring states for each state</returns>
         private void GenerateNodes(IPuzzleProcessor<MoveT, StateT> processor, StateT initialState, MoveT[] possibleMoves, out List<List<StateT>> neighbourMap, out List<List<MoveT>> neighbourMovesMap) {
             var statesToCheck = new Queue<StateT>();
@@ -37,7 +38,8 @@ namespace Wokarol.Pathfinder
             neighbourMovesMap = new List<List<MoveT>>();
 
             statesToCheck.Enqueue(initialState);
-            
+
+            // Looking through every possible state
             while (statesToCheck.Count > 0) {
                 var state = statesToCheck.Dequeue();
                 var node = new Node(state);
@@ -50,9 +52,10 @@ namespace Wokarol.Pathfinder
                 var neighbourMoves = new List<MoveT>();
                 foreach (var move in possibleMoves) {
                     var newState = processor.Process(state, move);
+                    // State is not the same as original one and therefore is move changes anything
                     if (!state.Equals(newState)) {
-                        if(!closedSet.Contains(newState) && !statesToCheck.Contains(newState)) {
-                            // State was not tested and is not in the queue
+                        // State was not tested and is not in the queue
+                        if (!closedSet.Contains(newState) && !statesToCheck.Contains(newState)) {
                             statesToCheck.Enqueue(newState);
                         }
                         neighbouringStates.Add(newState);
@@ -64,7 +67,12 @@ namespace Wokarol.Pathfinder
             }
         }
 
-        private void FindNodeNeighbours(List<List<StateT>> neighbourMap, List<List<MoveT>> neighbourMovesMap, MoveT[] possibleMoves) {
+        /// <summary>
+        /// Converts neighbourMap and neighbourMovesMap to Neighbours of Nodes in _graph
+        /// </summary>
+        /// <param name="neighbourMap">Map that corresponds to all states that are neighbour to given node, is same order as _graph</param>
+        /// <param name="neighbourMovesMap">Map that correcponds to all moves needed to get to neighbour states, same sizes as neighbourMap</param>
+        private void FindNodeNeighbours(List<List<StateT>> neighbourMap, List<List<MoveT>> neighbourMovesMap) {
             if (neighbourMap.Count != _graph.Count) throw new ArgumentException("Not mathing neighbour map");
             for (int i = 0; i < _graph.Count; i++) {
                 var node = _graph[i];
@@ -114,6 +122,9 @@ namespace Wokarol.Pathfinder
                 return this;
             }
 
+            /// <summary>
+            /// Represents a path to neighbouring state
+            /// </summary>
             public struct Neighbour
             {
                 public readonly Node Node;
