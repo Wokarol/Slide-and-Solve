2a1f25e23e9e78f9db5df4476bb16472c8fd7b06
Wojciech Karolczak <wojtek.karolczak20@gmail.com>
2019-03-24 18:43:49 +0100

    Added post processing; Added cinemachine; Solved small rendering issue with tilemap; Started working on MapDisplay scipt;

---
+++ Slide-and-Solve/Assets/Game/Scripts/Runtime/Tilemap/MapDisplay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Wokarol.TilemapUtils
+{
+    public class MapDisplay : MonoBehaviour
+    {
+        [SerializeField] Tilemap tilemap;
+
+        private void Start() {
+            UpdateMap();
+        }
+
+        private void UpdateMap() {
+            throw new NotImplementedException();
+        }
+    } 
+}
